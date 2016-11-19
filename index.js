5830a9e5b14048a627ae6e8df8c91fb7de69f156
Dennis Horte <dennis@fleksy.com>
2016-11-19 12:57:30 -0800

    Basic, non-persistent sessions.

--- index.js
+++ index.js
@@ -3,9 +3,33 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 
+var parseurl = require('parseurl')
+var session = require('express-session')
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(function (req, res, next) {
+  var views = req.session.views
+
+  if (!views) {
+    views = req.session.views = {}
+  }
+
+  // get the url pathname
+  var pathname = parseurl(req).pathname
+
+  // count the views
+  views[pathname] = (views[pathname] || 0) + 1
+
+  next()
+})
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -33,6 +57,14 @@ app.get('/db', function (request, response) {
         });
     });
 });
+
+app.get('/foo', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
+})
+
+app.get('/bar', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+})
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
