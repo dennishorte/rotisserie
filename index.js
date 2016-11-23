c1d0776b5420e9258f041a8d72518e892e02794f
Dennis Horte <dennis@fleksy.com>
2016-11-23 10:48:11 -0800

    Move routes into their own file.

--- index.js
+++ index.js
@@ -40,36 +40,7 @@ app.use(function (req, res, next) {
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-    response.render('pages/index');
-});
-
-app.get('/cool', function(request, response) {
-    response.send(cool());
-});
-
-app.get('/db', function (request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function(err, result) {
-            done();
-            if (err) {
-                console.error(err);
-                response.send("Error " + err);
-            }
-            else {
-                response.render('pages/db', {results: result.rows} );
-            }
-        });
-    });
-});
-
-app.get('/foo', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
-})
-
-app.get('/bar', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
-})
+require('./lib/routes.js')(app);
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
